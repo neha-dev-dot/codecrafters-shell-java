d2f00d4a61eeacd901061c238db4e43e18b14dbb
Neha Bharti <neetneha116@gmail.com>
2025-03-22 00:29:56 +0530

    Updated Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-        // System.out.print("$ ");
+        System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        Scanner Scanner = new Scanner(System.in);
+        String input = Scanner.nextLine();
     }
 }
